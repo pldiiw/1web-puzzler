0dd3c8288ef7076867df2dc14acf89b46d5bae59
PLD <pierre-louis@dourneau.me>
2016-12-19 23:07:22 +0100

    Add documentation and change imgURL var to picture

--- src/js/dragndrop.js
+++ src/js/dragndrop.js
@@ -1,6 +1,34 @@
 'use strict';
+
+// Custom event that will be called everytime a draggable is dropped
 const droppingEvent = new CustomEvent("dropping");
 
+/**
+ * Take every element that have a class recognized by the script and apply
+ * them the corresponding behaviour.
+ * The classes that are recognized by the script are:
+ *   .draggable - An element having that class can be dragged all over the
+ *                place.
+ *   .drop-anchor - A drop anchor is a kind of stop where when a draggable with
+ *                  the right classes is dropped onto, this one is tied to the
+ *                  drop anchor.
+ *   .draggable--drop-anchor-sensitive - Requires the draggable class. An
+ *                                       element with this class will react when
+ *                                       dropped onto a drop anchor.
+ *   .draggable--drop-anchor-exclusive - Requires the draggable class. Cannot be
+ *                                       used with the
+ *                                       draggable--drop-anchor-sensitive class
+ *                                       (no effect if so). The draggable will
+ *                                       only stay at the position it is
+ *                                       released when it is dropped onto a drop
+ *                                       anchor, otherwise it will return to its
+ *                                       original position.
+ *   .drop-anchor-max-elements-X - Where X is an integer. Requires the
+ *                                 drop-anchor class. A drop anchor with a such
+ *                                 class will only accept to tie a draggable to
+ *                                 itself if it does not have more than X
+ *                                 children.
+ */
 function setUpDraggables () {
   const draggables = document.querySelectorAll('.draggable');
   Array.prototype.forEach.call(draggables, makeDraggable);
@@ -11,6 +39,10 @@ function setUpDraggables () {
   Array.prototype.forEach.call(document.querySelectorAll('.drop-anchor'), v => v.style.zIndex = '998');
 }
 
+/**
+ * Make the element an object you can drag and drop.
+ * @param {DOMNode} element
+ */
 function makeDraggable (element) {
   element.style.zIndex = '999';
   element.addEventListener('mousedown', (event) => {
@@ -27,6 +59,12 @@ function makeDraggable (element) {
   });
 }
 
+/**
+ * Higher-order function that return the event listener to be used on mouse
+ * movement.
+ * @param {DOMEvent} baseEvent - The mouse down event triggered when the
+ *                               draggable was starting to be dragged.
+ */
 function dragger (baseEvent) {
   const layerX = baseEvent.layerX;
   const layerY = baseEvent.layerY;
@@ -38,11 +76,20 @@ function dragger (baseEvent) {
   };
 }
 
+/**
+ * Make element match the drop anchor sensitive behaviour.
+ * @param {DOMNode} element
+ */
 function makeAnchorSensitive (element) {
   const sensitive = sensitivizer(element);
   document.addEventListener('mouseup', sensitive);
 }
 
+/**
+ * Higher-order function that return the event listener to be used on mouse
+ * up, when the sensitive draggable is dropped.
+ * @param {DOMNode} element - The draggable to make sensitive.
+ */
 function sensitivizer (element) {
   return function (event) {
     if (element.classList.contains('dragging')) {
@@ -64,10 +111,20 @@ function sensitivizer (element) {
   };
 }
 
+/**
+ * Check if x and y are inside rect.
+ * @param {number} x
+ * @param {number} y
+ * @param {DOMRect} rect
+ */
 function areXYInsideRect (x, y, rect) {
   return y > rect.top && y < rect.bottom && x > rect.left && x < rect.right;
 }
 
+/**
+ * Return the maximum elements a drop-anchor can contain.
+ * @param {DOMNode} element - The drop anchor to examine.
+ */
 function getMaxElements (element) {
   const maxElementsClass = Array.prototype.reduce.call(element.classList,
     (a, v) => /drop-anchor--max-elements-[0-9]+/.test(v) ? v : a,
@@ -75,6 +132,10 @@ function getMaxElements (element) {
   return parseInt(maxElementsClass.slice(26));
 }
 
+/**
+ * Make element match the drop anchor exclusive behaviour.
+ * @param {DOMNode} element
+ */
 function makeAnchorExclusive (element) {
   if (!element.classList.contains('draggable--drop-anchor-sensitive')) {
     const sensitive = sensitivizer(element);
@@ -86,6 +147,7 @@ function makeAnchorExclusive (element) {
       }
     });
   } else {
+    // Warn if the element is already sensitive.
     console.warn('Warning: A draggable is both sensitive and exclusive to drop',
                  'anchors! Sensitiveness has priority over exclusiveness.');
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,34 @@
 'use strict';
 
+// Set images urls
 const pictures = [
   'media/octocat.jpg',
   'media/roses.jpg',
   'media/futura.jpg',
   'media/hongkong.jpg'
 ];
+// Start timer
 let timer = launchTimer();
 
+// Let user choose difficulty
 const difficultySelectionMenu = document.querySelector('#difficulty-selection-menu');
 const difficultyChoices = document.querySelectorAll('#difficulty-selection-menu div div');
 Array.prototype.forEach.call(difficultyChoices, (v, i) => {
+  // 3x3, 4x4 or 5x5
   if (i < 3) {
     v.addEventListener('click', () => {
+      // hide menu
       difficultySelectionMenu.style.animation = 'slideUp 0.8s ease-in-out forwards';
-      const choice = parseInt(v.attributes.value.value);
-      initializeGame(pickRandomArrayElt(pictures), choice, timer);
+      initializeGame(
+        pickRandomArrayElt(pictures),
+        parseInt(v.attributes.value.value),
+        timer
+      );
     });
+  // custom size
   } else {
     v.querySelector('p').addEventListener('click', () => {
+      // hide menu
       difficultySelectionMenu.style.animation = 'slideUp 0.8s ease-in-out forwards';
       initializeGame(
         pickRandomArrayElt(pictures),
@@ -29,15 +39,33 @@ Array.prototype.forEach.call(difficultyChoices, (v, i) => {
   }
 });
 
+/**
+ * Return a random element in an array.
+ * @param {Array} arr - The array from which the element is randomly selected.
+ */
 function pickRandomArrayElt (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+/**
+ * Set everything's up to let user player the game.
+ * @param {string} picture - The image the player will try to recreate.
+ * @param {number} puzzleSize - Number of pieces per side.
+ * @param {object} timer - The Timer object.
+ */
 function initializeGame (picture, puzzleSize, timer) {
   setUpGameBoard(picture, puzzleSize, timer);
+  // The game is starting, reset time to 0.
   timer.reset();
 }
 
+/**
+ * Call other procedures to get the game board ready and also the win condition
+ * detection.
+ * @param {string} picture - The image the player will try to recreate.
+ * @param {number} puzzleSize - Number of pieces per side.
+ * @param {object} timer - The Timer object.
+ */
 function setUpGameBoard (picture, puzzleSize, timer) {
   setUpPuzzlePicture(picture);
   setUpPuzzleToolbox(picture, puzzleSize);
@@ -46,18 +74,33 @@ function setUpGameBoard (picture, puzzleSize, timer) {
   setUpWinCondition(puzzleSize, timer);
 }
 
+/**
+ * Create a Timer object, set its timer function to be called every frame and
+ * return the object.
+ */
 function launchTimer () {
   let timer = Timer();
   window.requestAnimationFrame(timer.timer);
   return timer;
 }
 
+/**
+ * Add an event listener so that everytime a draggable is dropped, tries to
+ * get the register the score and reset the board with a new picture if the
+ * puzzle is complete. Globals are dirty, but didn't have time to get this right
+ * :(.
+ * @param {number} puzzleSize - Number of pieces per side.
+ * @param {object} timer - The Timer object.
+ */
 function setUpWinCondition (puzzleSize, timer) {
   document.addEventListener('dropping', () => {
     if (checkCompleteness()) {
+      // Get user time and reset timer
       const userTime = timer.reset();
-      UISay(`Yeah! You completed the puzzle in ${userTime} seconds!`);
+      // Congrats player
+      uiSay(`Yeah! You completed the puzzle in ${userTime} seconds!`);
 
+      // Retrieve other top scores and concatenate them with userTime.
       const scores = [userTime].concat(
         Array.prototype.reduce.call(
           document.querySelectorAll('#scoreboard ol li'),
@@ -66,11 +109,19 @@ function setUpWinCondition (puzzleSize, timer) {
         )
       );
       actualizeScoreboard(scores);
+
+      // New round!
       resetBoard(pickRandomArrayElt(pictures), puzzleSize, timer);
     }
   })
 }
 
+/**
+ * Given a list of scores, it sort out the 3 best ones and replace the
+ * scoreboard list with them.
+ * @param {number[]} scores - The scores from which the scoreboard will be
+ *                            set.
+ */
 function actualizeScoreboard (scores) {
   const scoreboardOl = document.querySelector('#scoreboard ol');
   const topScores = scores
@@ -86,20 +137,33 @@ function actualizeScoreboard (scores) {
   addChildren(scoreboardOl, topScores);
 }
 
+/**
+ * Remove all children from an element
+ * @param {DOMNode} element
+ */
 function removeAllChildren (element) {
   [...Array(element.children.length)].forEach(v => {
     element.removeChild(element.children[0]);
   });
 }
 
+/**
+ * Append all children to element
+ * @param {DOMNode} element
+ * @param {DOMNode[]} children
+ */
 function addChildren (element, children) {
   Array.prototype.forEach.call(children, v => {
     element.append(v);
   });
 }
 
+/**
+ * The Timer object used to manage the chronometer
+ */
 function Timer () {
   const chrono = document.querySelector('#chrono p');
+  // Last reset timestamp
   let lap = window.performance.now();
   const _timer = () => {
     chrono.innerText = ((window.performance.now() - lap) / 1000).toFixed(2);
@@ -113,11 +177,17 @@ function Timer () {
   return { timer: _timer, reset: _reset };
 }
 
-function UISay () {
+/**
+ * Add a message to the info board addressed to user.
+ * @param {...string} arguments - Concatenated, they will be the message that
+ *                                the user will see.
+ */
+function uiSay () {
   const messages = document.querySelector('#messages');
 
   Array.prototype.forEach.call(messages.children, (v, i) => {
-    v.style.opacity -= 0.34;
+    v.style.opacity -= 0.34; // Shade previous messages a bit
+    // Maximum three messages can be displayed at the same time
     if (i > 1) {
       messages.removeChild(v);
     }
@@ -129,15 +199,24 @@ function UISay () {
   msg.style.position = 'relative';
   msg.style.animation = 'newMessage 0.8s';
 
+  // Insert message atop the others
   messages.insertBefore(msg, messages.children[0]);
 }
 
-function getPieces (imgURL, puzzleSize) {
+/**
+ * Return an array of ready-to-be-inserted puzzle pieces made from picture.
+ * @param {string} picture - The image that will be cropped onto the pieces.
+ * @param {number} puzzleSize - Squared, it will be the number of pieces inside
+ *                              the returned array.
+ */
+function getPieces (picture, puzzleSize) {
   return [...Array(Math.pow(puzzleSize, 2))].map((v, i) => {
     const piece = document.createElement('div');
+    // The class piece-X is used to know which piece it is.
     piece.classList.add('w-100', 'h-100', 'draggable',
-      'draggable--drop-anchor-exclusive', 'relative', `piece-${i}`);
-    piece.style.backgroundImage = `url("${imgURL}")`;
+                        'draggable--drop-anchor-exclusive',
+                        'relative', `piece-${i}`);
+    piece.style.backgroundImage = `url("${picture}")`;
     piece.style.backgroundSize = `${100 * puzzleSize}% ${100 * puzzleSize}%`;
     piece.style.backgroundRepeat = 'no-repeat';
     const row = Math.floor(i / puzzleSize);
@@ -149,6 +228,10 @@ function getPieces (imgURL, puzzleSize) {
   });
 }
 
+/**
+ * Given an array, it will return a randomly rearranged version.
+ * @param {Array} arr - The array to scramble.
+ */
 function shuffleArray (arr) {
   if (arr.length > 0) {
     const n = Math.floor(Math.random() * arr.length);
@@ -160,29 +243,49 @@ function shuffleArray (arr) {
   }
 }
 
-function setUpPuzzleToolbox (imgURL, puzzleSize) {
+/**
+ * The toolbox is the area from which the player will drag out the pieces.
+ * @param {string} picture - The image the player will try to recreate.
+ * @param {number} puzzleSize - Number of pieces per side.
+ */
+function setUpPuzzleToolbox (picture, puzzleSize) {
   const puzzleToolbox = document.querySelector('#puzzle-toolbox');
 
   addDropAnchors(puzzleToolbox, puzzleSize);
 
-  const shuffledPieces = shuffleArray(getPieces(imgURL, puzzleSize));
+  const shuffledPieces = shuffleArray(getPieces(picture, puzzleSize));
   const dropAnchors = puzzleToolbox.querySelectorAll('.drop-anchor');
+  // Put the scrambled pieces onto the puzzle toolbox
   Array.prototype.forEach.call(dropAnchors, (v, i) => v.append(shuffledPieces[i]));
 }
 
+/**
+ * The puzzle board is the area where the player has to reconstruct the picture.
+ * @param {number} puzzleSize - Number of pieces per side.
+ */
 function setUpPuzzleBoard (puzzleSize) {
   const puzzleBoard = document.querySelector('#puzzle-board');
   addDropAnchors(puzzleBoard, puzzleSize);
 }
 
-function setUpPuzzlePicture (imgURL) {
+/**
+ * The puzzle picture is the area where the player can refer to when he wants to
+ * see what the picture looks like when put together back.
+ * @param {string} picture - The image the player will try to recreate.
+ */
+function setUpPuzzlePicture (picture) {
   const puzzlePicture = document.querySelector('#puzzle-picture');
-
-  puzzlePicture.style.backgroundImage = `url("${imgURL}")`;
+  puzzlePicture.style.backgroundImage = `url("${picture}")`;
   puzzlePicture.style.backgroundSize = '100% 100%';
   puzzlePicture.style.backgroundRepeat = 'no-repeat';
 }
 
+/**
+ * Put the puzzle squares inside element.
+ * @param {DOMNode} element - The element to put the puzzle squares inside.
+ * @param {number} puzzleSize - The number of rows and columns the puzzle will
+ *                              be composed of.
+ */
 function addDropAnchors (element, puzzleSize) {
   [...Array(puzzleSize)].forEach(() => {
     const row = document.createElement('div');
@@ -193,6 +296,7 @@ function addDropAnchors (element, puzzleSize) {
       const dropAnchor = document.createElement('div');
       dropAnchor.classList.add('drop-anchor', 'h-100', 'fl', 'ba', 'b--black-50', 'bw1');
       dropAnchor.style.width = `calc(100% / ${puzzleSize})`;
+
       row.append(dropAnchor);
     });
 
@@ -200,29 +304,48 @@ function addDropAnchors (element, puzzleSize) {
   });
 }
 
+/**
+ * Verify if all pieces are put in the right order on the puzzle board. If so,
+ * the puzzle is done.
+ */
 function checkCompleteness () {
   return Array.prototype.every.call(
     document.querySelectorAll('#puzzle-board .drop-anchor'),
-    (v, i) => Array.prototype.includes.call(v.children[0].classList, `piece-${i}`)
+    (v, i) => {
+      return v.children != undefined
+        ? Array.prototype.includes.call(v.children[0].classList, `piece-${i}`)
+        : false;
+    }
   );
 }
 
-function resetBoard (imgURL, puzzleSize) {
-  resetPuzzleToolbox(imgURL, puzzleSize);
+/**
+ * Reset the entire game board with picture and puzzleSize.
+ * @param {string} picture - The image the player will try to recreate.
+ * @param {number} puzzleSize - Number of pieces per side.
+ */
+function resetBoard (picture, puzzleSize) {
+  resetPuzzleToolbox(picture, puzzleSize);
   resetPuzzleBoard(puzzleSize);
-  setUpPuzzlePicture(imgURL, puzzleSize);
+  setUpPuzzlePicture(picture, puzzleSize);
   setUpDraggables();
 }
 
+/**
+ * Reset the puzzle board: remove all children and re-set it up.
+ * @param {number} puzzleSize - Number of pieces per side.
+ */
 function resetPuzzleBoard (puzzleSize) {
   removeAllChildren(document.querySelector('#puzzle-board'));
   setUpPuzzleBoard(puzzleSize);
 }
 
-function resetPuzzleToolbox (imgURL, puzzleSize) {
+/**
+ * Reset the puzzle toolbox: remove all children and re-set it up.
+ * @param {string} picture - The image the player will try to recreate.
+ * @param {number} puzzleSize - Number of pieces per side.
+ */
+function resetPuzzleToolbox (picture, puzzleSize) {
   removeAllChildren(document.querySelector('#puzzle-toolbox'));
-  setUpPuzzleToolbox(imgURL, puzzleSize);
+  setUpPuzzleToolbox(picture, puzzleSize);
 }
-
-// TODO: Doc
-// TODO: User choose difficulty
